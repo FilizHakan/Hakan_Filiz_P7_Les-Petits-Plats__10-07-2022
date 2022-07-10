6b520b75e83a7d3f5d3c79fcb9275900f52eb517
Filiz <filizhakan13@gmail.com>
2022-07-10 17:33:40 +0200

    Js name correction

--- scripts/Views/recipeCards.js
+++ scripts/Views/recipeCards.js
@@ -1,4 +1,4 @@
-export const createRecipeCards = (recipe) =>
+export const createRecipeCards = (id, name, number) =>
 {
     return `  
     <div class="dropdownContainer">
@@ -10,8 +10,8 @@ export const createRecipeCards = (recipe) =>
                 <input type="text" class="${id} displayNone" id="${id}" placeholder="Rechercher...">
                 <i class="bi bi-chevron-down arrowClose"></i>
             </div>
-            <div role="listbox" class="containerOption${nb} containerOption dropdown${name} displayNone">
-                <ul class="elementList${nb}"></ul>
+            <div role="listbox" class="containerOption${number} containerOption dropdown${name} displayNone">
+                <ul class="elementList${number}"></ul>
             </div>
         </div>
     </div>
