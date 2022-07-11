13f10bd7ecd9232a53ae4a5a2d46220734540fc8
Filiz <filizhakan13@gmail.com>
2022-07-11 15:14:30 +0200

    Changed html icon and bootstrap integration

--- scripts/views/RecipeCards.js
+++ scripts/views/RecipeCards.js
@@ -15,14 +15,17 @@ export default class RecipeCards {
   
       const recipeCard = `
                 <article class="container recipe overflow">
+
                     <!-- picture background - grey -->
                     <div class="row">
                       <div class="grey"></div>
                     </div>
+
                     <!-- recipe content -->
                     <div class="row">
                       <div class="recipe__content">
                         <div class="container">
+
                           <!-- Time and title -->
                           <div class="row">
                             <div class="col-6 col-sm-7 ps-2 pe-0">
@@ -31,10 +34,11 @@ export default class RecipeCards {
                             <div
                               class="offset-2 col-4  col-sm-5 offset-md-1 col-md-4 offset-lg-0 col-lg-5 text-end offset-xl-1 col-xl-4"
                             >
-                              <i class="fi fi-rr-clock"></i>
+                              <i class="far fa-clock"></i>
                               <h2 class="d-inline ms-2">${this._time} min</h2>
                             </div>
                           </div>
+
                           <!-- ingredients -->
                           <div class="row mt-2 pt-2">
                             <div class="col-lg-6 p-0 pe-2">
@@ -42,6 +46,7 @@ export default class RecipeCards {
                                 ${this._ingredients}
                               </ul>
                             </div>
+                            
                             <!-- Steps to follow -->
                             <div class=" col-lg-6 p-0 ">
                               <p class="recipe__description">
