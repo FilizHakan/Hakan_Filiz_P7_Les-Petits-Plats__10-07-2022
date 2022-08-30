dc63534f7898c2d424c547386b7198745947f2ff
Filiz <filizhakan13@gmail.com>
2022-08-30 22:10:52 +0200

    Algo 1

--- scripts/classes/Search.js
+++ scripts/classes/Search.js
@@ -8,58 +8,26 @@ export default class Search
         this.needle = normalise(needle, true);
     }
 
-    // 2nd Algo : Boucles Natives
-    goAlt()
+    // 1st Algo : Programmation Fonctionnelle
+    go() 
     {
 		console.log('******needle******')
 		console.log('needle :', this.needle)
-		console.time('algo 2')
-
-		let arrayRecipes = [];
-
-		for (let j = 0; j < this.recipes.length; j++)
+		console.time('algo 1')
+		
+		const arrayRecipes = this.recipes.filter((data) => 
 		{
-			const recipe = this.recipes[j];
+			let arrayIngredients = data.ingredients.map((el) => el.ingredient.toLowerCase());
 
-			if (this.isNeedleInTitleOrDescription(recipe) || this.isNeedleInIngredient(recipe))
-			{
-				arrayRecipes.push(recipe);
-			}	
-		}
+			return (
+			normalise(data.name).includes(this.needle) || // By title
+			normalise(data.description).includes(this.needle) || // By description
+			arrayIngredients.join().includes(this.needle) // By ingredients
+			);
 
-		console.timeEnd('algo 2')
-
-		return arrayRecipes;
+		});
+		console.timeEnd('algo 1');
+		return arrayRecipes;  
     }
 
-	isNeedleInIngredient(recipe)
-	{
-		let index = 0;
-
-		for (let i = 0; i < recipe.ingredients.length; i++)
-		{
-			if (recipe.ingredients[i].ingredient.toLowerCase().includes(normalise(this.needle, true))) // By ingredients
-			{
-				arrayRecipes[index] = recipe;
-				return true;
-			}
-		}
-
-		return false;
-	}
-
-	isNeedleInTitleOrDescription(recipe)
-	{
-		if (normalise(recipe.name).includes(normalise(this.needle, true))) 
-		{
-			return true;
-		} 
-
-		if (normalise(recipe.description).includes(normalise(this.needle, true))) // By description
-		{
-			return true;
-		}
-
-		return false;
-	}
 }
